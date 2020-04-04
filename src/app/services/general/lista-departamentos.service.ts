830ce05ca7a95b114b29497100f586f3315a6e43
JoMC98 <jordimc98@gmail.com>
2020-04-05 00:08:35 +0200

    s

--- src/app/services/general/lista-departamentos.service.ts
+++ src/app/services/general/lista-departamentos.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import departamentos from '../../files/departamentos.json';
+import departamentos from '../../../assets/files/departamentos.json';
 
 @Injectable({
   providedIn: 'root'
